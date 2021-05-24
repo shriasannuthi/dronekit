cc673e0df937ae637a534ef7469636327acc50ba
shriasannuthi <75028714+shriasannuthi@users.noreply.github.com>
2021-05-24 19:40:55 +0530

    Update hellodrone.py

--- hellodrone.py
+++ hellodrone.py
@@ -1,24 +1,21 @@
 
 print ("Start simulator (SITL)")
-import dronekit_sitl
-sitl = dronekit_sitl.start_default()
-connection_string = sitl.connection_string()
 
 # Import DroneKit-Python
-from dronekit import VehicleMode,connect
+from dronekit import connect, VehicleMode
 
 # Connect to the Vehicle.
-print(f"Connecting to vehicle on: {connection_string}")
-vehicle = connect(connection_string, wait_ready=True)
+print("Connecting to vehicle on: '127.0.0.1:14551'")
+vehicle = connect('127.0.0.1:14551', wait_ready=True)
 
 # Get some vehicle attributes (state)
 print ("Get some vehicle attribute values:")
-print (f" GPS: {vehicle.gps_0}")
-print (f" Battery: {vehicle.battery}")
-print (f" Last Heartbeat: {vehicle.last_heartbeat}")
-print (f" Is Armable?: {vehicle.is_armable}" )
-print (f" System status: {vehicle.system_status.state}" )
-print (f" Mode: {vehicle.mode.name}")   # settable
+print (" GPS: %s" % vehicle.gps_0)
+print (" Battery: %s" % vehicle.battery)
+print (" Last Heartbeat: %s" % vehicle.last_heartbeat)
+print (" Is Armable?: %s" % vehicle.is_armable)
+print (" System status: %s" % vehicle.system_status.state)
+print (" Mode: %s" % vehicle.mode.name)    # settable
 
 # Close vehicle object before exiting script
 vehicle.close()
